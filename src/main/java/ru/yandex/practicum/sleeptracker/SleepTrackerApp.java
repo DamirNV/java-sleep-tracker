9b7097e0566085ccee44fca3d70f528ea0d8f250
Damir Vafin <damirvafinnailevich@gmail.com>
2025-12-02 11:18:29 +0300

    feat: добавить enum SleepQuality для качества сна

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -1,5 +1,13 @@
 package ru.yandex.practicum.sleeptracker;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SleepTrackerApp {
 
     public static void main(String[] args) {
