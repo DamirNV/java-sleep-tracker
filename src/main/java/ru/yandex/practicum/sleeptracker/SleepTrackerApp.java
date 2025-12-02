415dcecaaa28121dc157d2e3980cdf2003381ba6
Damir Vafin <damirvafinnailevich@gmail.com>
2025-12-02 11:42:08 +0300

    feat: создать базовую структура основного класса приложения

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -6,11 +6,37 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SleepTrackerApp {
+    private List<SleepAnalysisFunction> analysisFunctions;
+
+    public SleepTrackerApp() {
+        this.analysisFunctions = new ArrayList<>();
+        // Позже добавим функции
+    }
+
+    public void addAnalysisFunction(SleepAnalysisFunction function) {
+        analysisFunctions.add(function);
+    }
 
     public static void main(String[] args) {
+        // Проверяем аргументы командной строки
+        if (args.length < 1) {
+            System.out.println("Использование: java SleepTrackerApp <путь_к_файлу>");
+            System.out.println("Пример: java SleepTrackerApp sleep_log.txt");
+            return;
+        }
 
+        String filePath = args[0];
+        System.out.println("Загружаем данные сна из файла: " + filePath);
+
+        try {
+            // Позже добавим загрузку файла
+            System.out.println("Функциональность загрузки файла будет добавлена на следующем шаге");
+
+        } catch (Exception e) {
+            System.err.println("Произошла ошибка: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
